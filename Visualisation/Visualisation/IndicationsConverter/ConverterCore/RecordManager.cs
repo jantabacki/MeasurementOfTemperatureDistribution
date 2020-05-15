85ad5a1ac23c83df2cf8b25cdbc3c0d3cd422177
jantabacki <jan.tabacki@gmail.com>
2020-05-15 18:23:27 +0200

    + simplified implementation of converter from measured analog values to Celsius degree temperature

---
+++ Visualisation/Visualisation/IndicationsConverter/ConverterCore/RecordManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using TemperatureIndicationLib;
+
+namespace IndicationsConverter.ConverterCore
+{
+    public static class RecordManager
+    {
+        public static List<TemperatureIndication> LoadRecord(this string filePath) 
+        {
+            FileStream stream = File.OpenRead(filePath);
+            BinaryFormatter formatter = new BinaryFormatter();
+            List<TemperatureIndication> temperatureIndications = (List<TemperatureIndication>)formatter.Deserialize(stream);
+            stream.Close();
+            return temperatureIndications;
+        }
+        
+        public static void SaveRecord(this List<TemperatureIndication> temperatureIndications, string filePath) 
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+            FileStream stream = File.Create(filePath);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, temperatureIndications);
+            stream.Close();
+        }
+    }
+}
---
+++ Visualisation/Visualisation/IndicationsConverter/Program.cs
@@ -0,0 +1,22 @@
+﻿using IndicationsConverter.ConverterCore;
+using System.Collections.Generic;
+using TemperatureIndicationLib;
+
+namespace IndicationsConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TemperatureIndication> temperatureIndications = args[0].LoadRecord();
+            List<TemperatureIndication> outputRecord = new List<TemperatureIndication>();
+            Converter converter = new Converter();
+            foreach (TemperatureIndication temperatureIndication in temperatureIndications)
+            {
+                TemperatureIndication modifiedRecord = new TemperatureIndication(temperatureIndication.DateTime, temperatureIndication.PosX, temperatureIndication.PosY, converter.convertValue(temperatureIndication.Value));
+                outputRecord.Add(modifiedRecord);
+            }
+            outputRecord.SaveRecord(args[0] + "Converted");
+        }
+    }
+}
